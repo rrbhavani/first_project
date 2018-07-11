b6f93aa04cdf2a9242226e40b53785b4107253f7
Bhavani <r.r.bhavani@gmail.com>
2018-07-11 23:02:17 +0530

    commit in new branch

--- FirstClass.java
+++ FirstClass.java
@@ -2,6 +2,7 @@ public class FirstClass {
 	public static void main(String args[]){
 		System.out.println("My first class");
 		System.out.println("My second commit");
+		System.out.println("I am in new branch");
 	}
 }
 
